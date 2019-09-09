aeb5b098feb23c0b0ed018e2d350e3c8bbeb3c34
Serge Nikulin <serge@safeai.ai>
2019-09-09 09:30:37 -0700

    Address revew findings
    Signed-off-by: Serge Nikulin <serge@safeai.ai>

--- ament_cmake_version/cmake/ament_cmake_version.cmake
+++ ament_cmake_version/cmake/ament_cmake_version.cmake
@@ -1,104 +1,67 @@
 ################################################################################
 # `ament_cmake_version` function creates and installs a version header file.
 # The version is taken from `package.xml` file's `<version>` tag
-# The function uses an existing "resource/version.h.in" template file to generate
+# The function uses a provided "version.h.in" template file to generate
 # the destination `version.h` file and installs that generated file into `DESTINATION include`.
 # The generated file is being (re-)created if:
 # - the file does not exist
 # - the file does exists but contains a version that differs from the version in `package.xml` file
 ################################################################################
-# Example of a template file for `rcutils` project:
-# // Copyright 2015 Open Source Robotics Foundation, Inc.
-# //
-# // Licensed under the Apache License, Version 2.0 (the "License");
-# // you may not use this file except in compliance with the License.
-# // You may obtain a copy of the License at
-# //
-# //     http://www.apache.org/licenses/LICENSE-2.0
-# //
-# // Unless required by applicable law or agreed to in writing, software
-# // distributed under the License is distributed on an "AS IS" BASIS,
-# // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# // See the License for the specific language governing permissions and
-# // limitations under the License.
-#
-# #ifndef RCUTILS__VERSION_H_
-# #define RCUTILS__VERSION_H_
-#
-# /// \def RCUTILS_VERSION_MAJOR
-# /// Defines RCUTILS major version number
-# #define RCUTILS_VERSION_MAJOR (@rcutils_VERSION_MAJOR@)
-#
-# /// \def RCUTILS_VERSION_MINOR
-# /// Defines RCUTILS minor version number
-# #define RCUTILS_VERSION_MINOR (@rcutils_VERSION_MINOR@)
-#
-# /// \def RCUTILS_VERSION_PATCH
-# /// Defines RCUTILS version patch number
-# #define RCUTILS_VERSION_PATCH (@rcutils_VERSION_PATCH@)
-#
-# /// \def RCUTILS_VERSION_STR
-# /// Defines RCUTILS version string
-# #define RCUTILS_VERSION_STR "@rcutils_VERSION@"
-#
-# #endif  // RCUTILS__VERSION_H_"
-
 function(ament_cmake_version)
+  if (NOT ${ARGC} EQUAL 0)
+    message(FATAL_ERROR "ament_cmake_version does not accept parameters but ${ARGC} parameters provided")
+  endif()
+
+  # Find myself so I can use my own cmake instalation folder `ament_cmake_version_DIR`
+  # I can't rely on a pervious find_package call because it could be a direct call
+  find_package(ament_cmake_version REQUIRED)
+  set(TEMPLATE_FILE "${ament_cmake_version_DIR}/version.h.in")
+  if(NOT EXISTS "${TEMPLATE_FILE}")
+    message(FATAL_ERROR "Can't find ${TEMPLATE_FILE}. Reinstall ament_cmake_version package.")
+  endif()
+
   include_directories(${CMAKE_CURRENT_BINARY_DIR}/ament_cmake_version/include)
   set(TMP_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/ament_cmake_version/include/${PROJECT_NAME})
 
-  set(${PROJECT_NAME}_VERSION_FILE_NAME ${TMP_INCLUDE_DIR}/version.h)
-  set(NEED_TO_CREATE_VERSION_FILE FALSE)
+  set(VERSION_FILE_NAME ${TMP_INCLUDE_DIR}/version.h)
+  set(NEED_TO_CREATE_VERSION_FILE TRUE)
 
-  # retrieve verson information from <package>.xml file
+  # retrieve version information from <package>.xml file
   # call ament_package_xml() if it has not been called before
   if(NOT _AMENT_PACKAGE_NAME)
     ament_package_xml()
   endif()
 
-  # parse version information from the version string
-  string(REGEX MATCH "([0-9]+\).([0-9]+)\.([0-9]+)" "" dummy ${${PROJECT_NAME}_VERSION})
-  set(${PROJECT_NAME}_VERSION_MAJOR ${CMAKE_MATCH_1})
-  set(${PROJECT_NAME}_VERSION_MINOR ${CMAKE_MATCH_2})
-  set(${PROJECT_NAME}_VERSION_PATCH ${CMAKE_MATCH_3})
+  string(TOUPPER ${PROJECT_NAME} PROJECT_NAME_UPPER)
+  set(VERSION_STR ${${PROJECT_NAME}_VERSION})
 
-  # sanity checks
-  if("${${PROJECT_NAME}_VERSION_MAJOR}" STREQUAL "")
-    message(FATAL_ERROR "${PROJECT_NAME}_VERSION_MAJOR is empty or non-numeric, check <package>.XML")
-  endif()
-  if("${${PROJECT_NAME}_VERSION_MINOR}" STREQUAL "")
-    message(FATAL_ERROR "${PROJECT_NAME}_VERSION_MINOR is empty or non-numeric, check <package>.XML")
-  endif()
-  if("${${PROJECT_NAME}_VERSION_PATCH}" STREQUAL "")
-    message(FATAL_ERROR "${PROJECT_NAME}_VERSION_PATCH is empty or non-numeric, check <package>.XML")
-  endif()
+  # parse version information from the version string
+  string(REGEX MATCH "([0-9]+)\.([0-9]+)\.([0-9]+)" "" dummy ${VERSION_STR})
+  set(VERSION_MAJOR ${CMAKE_MATCH_1})
+  set(VERSION_MINOR ${CMAKE_MATCH_2})
+  set(VERSION_PATCH ${CMAKE_MATCH_3})
 
   # Check if the version file exist
-  if(EXISTS "${${PROJECT_NAME}_VERSION_FILE_NAME}")
+  if(EXISTS "${VERSION_FILE_NAME}")
     # The file exists
     # Check if it contains the same version
-    file(STRINGS ${${PROJECT_NAME}_VERSION_FILE_NAME} VERSION_FILE_STRINGS REGEX "#define ${PROJECT_NAME}_VERSION_STR")
-    if(VERSION_FILE_STRINGS STREQUAL "")
-      set(NEED_TO_CREATE_VERSION_FILE TRUE)
-    else()
-      string(REGEX MATCH "^#define[ \t]+${PROJECT_NAME}_VERSION_STR[ \t]+\"([0-9]+\.[0-9]+\.[0-9]+)\"" "" dummy ${VERSION_FILE_STRINGS})
-      if(NOT (${CMAKE_MATCH_1} STREQUAL ${${PROJECT_NAME}_VERSION}))
-        # Create new file if file version != ${PROJECT_NAME}_VERSION
-        set(NEED_TO_CREATE_VERSION_FILE TRUE)
-      endif()
+    set(LINE_PATTERN "#define[ \t]+${PROJECT_NAME_UPPER}_VERSION_STR")
+    file(STRINGS ${VERSION_FILE_NAME} VERSION_FILE_STRINGS REGEX ${LINE_PATTERN})
+    set(VERSION_PATTERN "^#define[ \t]+${PROJECT_NAME_UPPER}_VERSION_STR[ \t]+\"([0-9]+\.[0-9]+\.[0-9]+)\"")
+    string(REGEX MATCH ${VERSION_PATTERN} dummy ${VERSION_FILE_STRINGS})
+    if("${CMAKE_MATCH_1}" STREQUAL "${VERSION_STR}")
+      message(STATUS "File and project versions match: \"${CMAKE_MATCH_1}\" == \"${VERSION_STR}\"")
+      set(NEED_TO_CREATE_VERSION_FILE FALSE)
     endif()
-  else()
-    # if the version file does not exist, create it
-    set(NEED_TO_CREATE_VERSION_FILE TRUE)
   endif()
 
   if(${NEED_TO_CREATE_VERSION_FILE})
     message(STATUS "Create new version file for version ${${PROJECT_NAME}_VERSION}")
     file(MAKE_DIRECTORY ${TMP_INCLUDE_DIR})
     # create the version.h file
-    configure_file("resource/version.h.in" ${${PROJECT_NAME}_VERSION_FILE_NAME} NEWLINE_STYLE UNIX)
+    configure_file(${TEMPLATE_FILE} ${VERSION_FILE_NAME})
   else()
-      message(STATUS "Skip version file creation")
+    message(STATUS "Skip version file creation")
   endif()
 
   install(
